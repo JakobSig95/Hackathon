817069fdbf34c995b063c9faae3eb8313c020954
JakobSig95 <Jakobdransfeldt@gmail.com>
2022-09-14 15:23:24 +0200

    Update streamlit_app.py

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -2,6 +2,10 @@
 import streamlit as st
 import numpy as np # Basic library for all kind of numerical operations
 import pandas as pd # Basic library for data manipulation in dataframes
+import matplotlib.pyplot as plt # comand for ploting
+import seaborn as sns; sns.set() # this is a data visualization library built on top of matplotlib
+from matplotlib.patches import ConnectionPatch # using this for later when zooming
+import plotly.express as px # Plotly plots
 
 st.title('Shark Attacks')
 

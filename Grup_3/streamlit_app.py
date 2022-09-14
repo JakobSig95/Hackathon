fcc6f9945ff64ac0e5d405cf663e81d7cd243345
JakobSig95 <Jakobdransfeldt@gmail.com>
2022-09-14 15:37:01 +0200

    Update streamlit_app.py

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -1,17 +1,17 @@
 
 import streamlit as st
-import numpy as np # Basic library for all kind of numerical operations
-import pandas as pd # Basic library for data manipulation in dataframes
-import matplotlib.pyplot as plt # comand for ploting
-import seaborn as sns; sns.set() # this is a data visualization library built on top of matplotlib
-from matplotlib.patches import ConnectionPatch # using this for later when zooming
-import plotly.express as px # Plotly plots
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns; sns.set()
+from matplotlib.patches import ConnectionPatch
+import plotly.express as px
 
 st.title('Shark Attacks')
 
 data = pd.read_csv('https://raw.githubusercontent.com/Alphambarushimana/Grup_3/main/attacks.csv', encoding='iso8859-1')
 
-data #showing the data
+data
 
 data.info()
 
@@ -23,13 +23,9 @@ data.info()
 
 data.columns
 
-# remove the space in the column name for better syntax and readability
-
 data = data.rename(columns = {'Sex ':'Gender'})
 
 data.Gender.unique()
-
-# sort the unique values in column Gender into three categories: Female, Male and Unknown
 
 data['Gender'] =  data['Gender'].replace(['M','M ', 'N'],'Male')
 data['Gender'] =  data['Gender'].replace(['F'],'Female')
@@ -38,7 +34,6 @@ data['Gender'] = data['Gender'].fillna('Unknown')
 
 data.Gender.value_counts()
 
-#from 1900 to 2018
 sex_attacks = data.groupby('Gender')['Gender'].count()
 sex_attacks = sex_attacks[(sex_attacks.index == 'Male') | (sex_attacks.index=='Female')|(sex_attacks.index=='Unknown')]
 sex_attacks
@@ -47,7 +42,6 @@ fig = px.pie(sex_attacks, values=sex_attacks.values, names=sex_attacks.index, ti
 fig.update_layout(height=500, width=600)
 fig.show()
 
-#from 2010 - 2018 
 sex_attacks_2010_2018 = data[data.Year >= 2010].groupby('Gender')['Gender'].count()
 sex_attacks_2010_2018 = sex_attacks_2010_2018[(sex_attacks_2010_2018.index == 'Male') | (sex_attacks_2010_2018.index =='Female')| (sex_attacks_2010_2018.index=='Unknown')]
 sex_attacks
@@ -62,8 +56,6 @@ data.Year.unique()
 data = data[data['Year'] >= 1900]
 
 data.Year.unique()
-
-# Test plot
 
 byYear_attack = data.groupby('Year')['Date'].count().reset_index()
 fig = px.line(byYear_attack,x='Year', y='Date', title='Shark Attack by Year')

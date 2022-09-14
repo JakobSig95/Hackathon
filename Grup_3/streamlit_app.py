d2c1088d7c6c2d5664e8563b151989ad2b28eff9
JakobSig95 <Jakobdransfeldt@gmail.com>
2022-09-14 16:29:07 +0200

    Update streamlit_app.py

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -21,10 +21,47 @@ with st.sidebar:
         ("Standard (5-15 days)", "Express (2-5 days)")
     )
 
+data = pd.read_csv('https://raw.githubusercontent.com/Alphambarushimana/Grup_3/main/attacks.csv', encoding='iso8859-1')
+
+data #showing the data
+
+data.info()
+
+data.isna().sum()
+
+data.drop(['Case Number', 'Name', 'Injury', 'Time', 'Investigator or Source', 'pdf', 'href formula', 'href', 'Case Number.1', 'Case Number.2', 'original order', 'Unnamed: 22', 'Unnamed: 23'], axis = 1, inplace = True)
+
+data.info()
+
+data.columns
+
+# remove the space in the column name for better syntax and readability
+
+data = data.rename(columns = {'Sex ':'Gender'})
+
+data.Gender.unique()
+
+# sort the unique values in column Gender into three categories: Female, Male and Unknown
+
+data['Gender'] =  data['Gender'].replace(['M','M ', 'N'],'Male')
+data['Gender'] =  data['Gender'].replace(['F'],'Female')
+data['Gender'] =  data['Gender'].replace(['.','lli'],'Unknown')
+data['Gender'] = data['Gender'].fillna('Unknown')
+
+data.Gender.value_counts()
+
+#from 1900 to 2018
+sex_attacks = data.groupby('Gender')['Gender'].count()
+sex_attacks = sex_attacks[(sex_attacks.index == 'Male') | (sex_attacks.index=='Female')|(sex_attacks.index=='Unknown')]
+sex_attacks
+
+fig = px.pie(sex_attacks, values=sex_attacks.values, names=sex_attacks.index, title='Shark Attacks by Gender')
+fig.update_layout(height=500, width=600)
+fig.show()
+      
 fig = px.pie(sex_attacks, values=sex_attacks.values, names=sex_attacks.index, title='Shark Attacks by Gender')
 fig.update_layout(height=500, width=600)
 st.plotly_chart(fig, use_container_width=True)
-
 
 #from 2010 - 2018 
 sex_attacks_2010_2018 = data[data.Year >= 2010].groupby('Gender')['Gender'].count()
@@ -47,8 +84,6 @@ data.Year.unique()
 byYear_attack = data.groupby('Year')['Date'].count().reset_index()
 fig = px.line(byYear_attack,x='Year', y='Date', title='Shark Attack by Year')
 fig.show()
-
-
 
 ## Activities
 

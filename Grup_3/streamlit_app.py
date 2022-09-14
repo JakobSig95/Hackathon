500ef4223a0c3db760f62e11551d07f4476a07a6
JakobSig95 <Jakobdransfeldt@gmail.com>
2022-09-14 16:54:45 +0200

    Jakob made changes

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -7,24 +7,11 @@ import seaborn as sns; sns.set()
 from matplotlib.patches import ConnectionPatch
 import plotly.express as px
 
-st.title('Shark Attacks')
-
-
-tab1, tab2, tab3 = st.tabs(["Pie", "Map", "Line"])
-fig = [tab1]
-
-with tab1:
-   st.header("A Pie")
-   
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
-    )
-
 data = pd.read_csv('https://raw.githubusercontent.com/Alphambarushimana/Grup_3/main/attacks.csv', encoding='iso8859-1')
 
 data #showing the data
+
+Cleaning the data
 
 data.info()
 
@@ -35,6 +22,7 @@ data.drop(['Case Number', 'Name', 'Injury', 'Time', 'Investigator or Source', 'p
 data.info()
 
 data.columns
+
 
 # remove the space in the column name for better syntax and readability
 
@@ -59,10 +47,6 @@ sex_attacks
 fig = px.pie(sex_attacks, values=sex_attacks.values, names=sex_attacks.index, title='Shark Attacks by Gender')
 fig.update_layout(height=500, width=600)
 fig.show()
-      
-fig = px.pie(sex_attacks, values=sex_attacks.values, names=sex_attacks.index, title='Shark Attacks by Gender')
-fig.update_layout(height=500, width=600)
-st.plotly_chart(fig, use_container_width=True)
 
 #from 2010 - 2018 
 sex_attacks_2010_2018 = data[data.Year >= 2010].groupby('Gender')['Gender'].count()
@@ -73,6 +57,7 @@ fig = px.pie(sex_attacks_2010_2018, values=sex_attacks_2010_2018.values, names=s
              title='Shark Attacks by Gender - Between 2010-2018')
 fig.update_layout(height=500, width=600)
 fig.show()
+
 
 data.Year.unique()
 
@@ -85,6 +70,8 @@ data.Year.unique()
 byYear_attack = data.groupby('Year')['Date'].count().reset_index()
 fig = px.line(byYear_attack,x='Year', y='Date', title='Shark Attack by Year')
 fig.show()
+
+
 
 ## Activities
 
@@ -112,6 +99,10 @@ fig = px.bar(prov_activity, x=prov_activity.values, y=prov_activity.index, orien
 fig.update_layout(height=600, width=900)
 fig.show()
 
+
+
+# Fatality
+
 # remove the space in the column name for better syntax and readability
 
 data = data.rename(columns = {'Fatal (Y/N)':'Fatality'})
@@ -126,3 +117,72 @@ data['Fatality'] =  data['Fatality'].replace(['UNKNOWN', 'M', '2017'],'Unknown')
 data['Fatality'] = data['Fatality'].fillna('Unknown')
 
 data.Fatality.value_counts()
+
+data.info()
+
+Mydata = data.groupby(['Fatality', 'Gender'], as_index=False).size()
+Mydata = Mydata.sort_values(by=['size'], ascending=False)
+Mydata = Mydata[0:7]
+Mydata.drop([5],inplace=True)
+
+Mydata.drop([4],inplace=True)
+Mydata.drop([6],inplace=True)
+Mydata.head()
+
+import plotly.express as px
+mlabels=['Male Non Fatal', 'Male fatael','Female Non Fatal','Female Fatal']
+fig = px.pie(Mydata, names=mlabels,values='size',hole = 0.8)
+fig.update_traces(textposition='outside', textinfo='percent+label')
+fig.update_layout(
+    annotations=[dict(text="comparison of accidents", x=0.5, y=0.5, font_size=20, showarrow=False)])
+fig.update_layout(showlegend=False)
+
+# Location
+
+len(data.Country.unique())
+
+# Top five countries with most shark attacks
+
+data['Country'].value_counts().head(5)
+
+# Top five countries with least shark attacks
+
+data['Country'].value_counts().tail(5)
+
+# Attacks by country
+
+attacks_by_country = data['Country'].value_counts().reset_index().rename(columns={'Country':'Count','index':'Country'})
+attacks_by_country.head()
+
+# World map of attacks by country
+world_map = px.choropleth(attacks_by_country,
+                    locations = 'Country',
+                    color = 'Count',
+                    color_continuous_scale='Plasma',
+                    locationmode = 'country names',
+                    scope = 'world',
+                    title = 'Shark attacks around the World',
+                    labels = {'Count':'Shark attacks'}
+                    )
+
+world_map.update_geos(fitbounds="locations", visible=False)
+world_map.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+world_map.show()
+
+
+st.title('Shark Attacks')
+
+
+tab1, tab2, tab3 = st.tabs(["Pie", "Map", "Line"])
+fig = [tab1]
+
+with tab1:
+   st.header("A Pie")
+   
+with st.sidebar:
+    add_radio = st.radio(
+        "Choose a shipping method",
+        ("Standard (5-15 days)", "Express (2-5 days)")
+    )
+

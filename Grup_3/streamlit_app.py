874b0cc4eb21128e2eaf40bcea00fc347ded0157
JakobSig95 <Jakobdransfeldt@gmail.com>
2022-09-14 15:14:28 +0200

    Update streamlit_app.py

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -5,3 +5,10 @@ import pandas as pd # Basic library for data manipulation in dataframes
 
 st.title('Shark Attacks')
 
+sex_attacks = data.groupby('Gender')['Gender'].count()
+sex_attacks = sex_attacks[(sex_attacks.index == 'Male') | (sex_attacks.index=='Female')|(sex_attacks.index=='Unknown')]
+sex_attacks
+
+fig = px.pie(sex_attacks, values=sex_attacks.values, names=sex_attacks.index, title='Shark Attacks by Gender')
+fig.update_layout(height=500, width=600)
+fig.show()

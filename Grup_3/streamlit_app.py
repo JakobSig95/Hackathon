fb9e99e8653a05d0f100b7bed57bbfa3801cb17b
JakobSig95 <Jakobdransfeldt@gmail.com>
2022-09-14 15:13:04 +0200

    Update streamlit_app.py

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -2,7 +2,6 @@
 import streamlit as st
 import numpy as np # Basic library for all kind of numerical operations
 import pandas as pd # Basic library for data manipulation in dataframes
-import matplotlib.pyplot as plt # comand for ploting
 from matplotlib.patches import ConnectionPatch # using this for later when zooming
 import plotly.express as px # Plotly plots
 

40b004a96b899f7680a429b758ac9952442c17df
JakobSig95 <Jakobdransfeldt@gmail.com>
2022-09-14 14:54:33 +0200

    Update streamlit_app.py

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -5,11 +5,6 @@ st.title('Shark Attacks')
 
 import numpy as np # Basic library for all kind of numerical operations
 import pandas as pd # Basic library for data manipulation in dataframes
-import matplotlib.pyplot as plt # comand for ploting
-import seaborn as sns; sns.set() # this is a data visualization library built on top of matplotlib
-
-from matplotlib.patches import ConnectionPatch # using this for later when zooming
-import plotly.express as px # Plotly plots
 
 data = pd.read_csv('https://raw.githubusercontent.com/Alphambarushimana/Grup_3/main/attacks.csv', encoding='iso8859-1')
 

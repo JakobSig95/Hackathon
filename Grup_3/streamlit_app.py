ab1c070d4e793de1871fa8bcfb29c88e5ecd83b1
JakobSig95 <Jakobdransfeldt@gmail.com>
2022-09-14 16:13:17 +0200

    Update streamlit_app.py

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -9,6 +9,14 @@ import plotly.express as px
 
 st.title('Shark Attacks')
 
+tab1, tab2, tab3 = st.tabs(["Pie", "Map", "Line"])
+
+with tab1:
+   st.header("A Pie")
+   fig = px.pie(sex_attacks, values=sex_attacks.values, names=sex_attacks.index, title='Shark Attacks by Gender')
+fig.update_layout(height=500, width=600)
+st.plotly_chart(fig, use_container_width=True)
+
 with st.sidebar:
     add_radio = st.radio(
         "Choose a shipping method",

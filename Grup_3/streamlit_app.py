63cc192010d7baa64c69e363611ac9b3eca06c54
Brlsadiksha <113443083+Brlsadiksha@users.noreply.github.com>
2022-09-15 11:51:50 +0200

    ihij

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -176,10 +176,10 @@ with tab2:
                 It seems the sharks need to attend shark-university to become better predators. Almost 75% of their attacks are non-fatal
             """)
 
-    values = st.slider(
+    Gender = st.slider(
         'Select a range of values',
          0.0, 100.0, (25.0, 75.0))
-     st.write('Year:', values)
+     st.write('Year:', Gender)
 
 
 

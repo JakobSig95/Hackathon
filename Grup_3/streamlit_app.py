ec542934f02aff79a19ac4c9cf1b34e4653f91e2
Brlsadiksha <113443083+Brlsadiksha@users.noreply.github.com>
2022-09-15 11:32:40 +0200

    yuvvoug

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -176,13 +176,13 @@ with tab2:
                 It seems the sharks need to attend shark-university to become better predators. Almost 75% of their attacks are non-fatal
             """)
 
-Gender = st.slider('which year', 0, 100, 100)
-st.write("gender attacks")
+    Gender = st.slider('which year', 0, 100, 100)
+    st.write("gender attacks")
 
-values = st.slider(
-    'Select a range of values',
-    0.0, 100.0, (1990, 2018))
-st.write('Values:', values)
+    values = st.slider(
+       'Select a range of values',
+        0.0, 100.0, (1990, 2018))
+    st.write('Values:', values)
 
 
 

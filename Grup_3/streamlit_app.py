3f027b57fa28c50ba2d6901b3df3a5ceacc2c9ad
Brlsadiksha <113443083+Brlsadiksha@users.noreply.github.com>
2022-09-15 11:34:36 +0200

    78t8hph

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -179,9 +179,10 @@ with tab2:
     Gender = st.slider('which year', 0, 100, 50)
     st.write("gender attacks")
 
-    values = st.slider(
-       'Select a range of values',
-        0.0, 100.0, (1990, 2018))
+         values = st.slider(
+             'Select a range of values',
+              0.0, 100.0, (1990, 2018)
+              format="MM/DD/YY - hh:mm"))
     st.write('Values:', values)
 
 

c6da73f1fceb6489e12647263b4551d4dd5414ca
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-09-15 10:40:20 +0200

    hj

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -109,7 +109,6 @@ attacks_by_country.head()
 world_map = px.choropleth(attacks_by_country,
                     locations = 'Country',
                     color = 'Count',
-                    color_continuous_scale='Plasma',
                     locationmode = 'country names',
                     scope = 'world',
                     title = 'Shark attacks around the World',

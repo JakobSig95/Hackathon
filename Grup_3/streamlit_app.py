91c2fab75e3d5308739870c57c13cccd506b94b6
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-09-15 09:11:12 +0200

    ldkjklfsd

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -142,12 +142,12 @@ with tab1:
         col1, col2 = st.columns(2)
 
         with col1:
-        st.header("Attacks by Gender")
-        st.plotly_chart(gender_fig, use_container_width=True)
+            st.header("Attacks by Gender")
+            st.plotly_chart(gender_fig, use_container_width=True)
 
         with col2:
-        st.header("Fatality by Gender")
-        st.plotly_chart(fatality_fig, use_container_width=True)
+            st.header("Fatality by Gender")
+            st.plotly_chart(fatality_fig, use_container_width=True)
 
 
 with tab2:

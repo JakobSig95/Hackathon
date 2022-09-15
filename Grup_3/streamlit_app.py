1e3c73b60af4b77119a378fc1c8aa4ea89dce27d
Nadia <112628369+NadiaHolmlund@users.noreply.github.com>
2022-09-15 11:35:01 +0200

    head

--- Grup_3/streamlit_app.py
+++ Grup_3/streamlit_app.py
@@ -131,10 +131,10 @@ st.title('Shark Attacks 🦈')
 tab1, tab2, tab3, tab4 = st.tabs(["Introduction", "Gender and fatality", "Attacks around the world", "Activities and year"])
 
 with tab1:
-    st.header('Introduction to Shark Attacks')
+    st.header('Introduction to the dashboard')
     st.text('Every year, shark attacks occur around the world. Despite their rarity, many people fear attacks when going into water.')
     st.text('By understanding when, where and during which activities shark attacks occur, it is possible to lessen the likelihood')
-    st.text('and fear of these incidents.')
+    st.text('and thereby fear of these incidents.')
     st.text('')
     st.text('Browse the tabs to get insightful information about shark attacks.')
     st.text('')
